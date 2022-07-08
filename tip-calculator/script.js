b93e32aabde965411c69358831ba7753e773fed6
valentim <pedamastim@gmail.com>
2022-07-08 18:23:37 -0300

    improve script

--- tip-calculator/script.js
+++ tip-calculator/script.js
@@ -1,24 +1,16 @@
 // calculate tip %
 // return tip and total bill value with tip
 
-let inputBill = document.getElementById('inputBill');
-let inputTip = document.getElementById('inputTip');
-console.log(inputBill);
-console.log(inputTip);
-let button = document.getElementById('button');
-console.log(button);
-/* button.addEventListener('click', calc()) */
+const inputBill = document.querySelector('#inputBill');
+const inputTip = document.querySelector('#inputTip');
+const button = document.querySelector('#button');
 
 function calc(event){
     event.preventDefault();
-    let bill= parseInt(inputBill.value);
-    console.log(bill);
-    let tip = parseInt(inputTip.value);
-    console.log(tip);
+    let bill= (parseInt(inputBill.value));
+    let tip = (parseInt(inputTip.value));
     let tipTotal = bill * (tip / 100); //tip value
-    console.log(tipTotal)
     let billTotal = bill + tipTotal; //total bill with tip
-    console.log(billTotal);
     let result = document.getElementById('result');
     result.innerHTML = `<p>Your tip amount: ${tipTotal}</p>`
     result.innerHTML += `<p>Your total bill (with tip): ${billTotal}</p>`
